93c0339b55cfd822d129e1624b10b4d9a09c32fd
Todd Fields <toddwfields@gmail.com>
2016-02-26 12:29:31 -0600

    Initial Commit

---
+++ SkullgateShowcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkullgateShowcase
+//
+//  Created by Todd Fields on 2016-02-26.
+//  Copyright © 2016 Skullgate Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
